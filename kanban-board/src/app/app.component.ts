07c53c3181e5544c0688612df2364889a112e073
adityathakkar17 <adityathakkar33@gmail.com>
2023-02-19 00:58:54 +0530

    Added drag and drop module

--- kanban-board/src/app/app.component.ts
+++ kanban-board/src/app/app.component.ts
@@ -1,10 +1,32 @@
+import { CdkDragDrop, transferArrayItem, CdkDrag } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
-
+import { Task } from './task/task';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'kanban-board';
+  todo: Task[] = [
+    {title: 'PLAT-12',description:'GOOD'},
+    {title: 'PLAT-34',description:'BAD'},
+  ];
+  inProgress: Task[] = [];
+  done: Task[] = [];
+
+  drop(event: CdkDragDrop<Task[]>): void {
+    if(event.previousContainer === event.container) {
+      return;
+    }
+    transferArrayItem(
+      event.previousContainer.data,
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex
+    )
+  }
+
+  edit(list:string, task:Task): void {
+
+  }
 }
