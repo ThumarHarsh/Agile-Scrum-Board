e010fe5ec3fee7137b4f70675db33a34eeebbb21
adityathakkar17 <adityathakkar33@gmail.com>
2023-02-19 23:47:48 +0530

    Added code for Add,Edit,Delete Task

--- kanban-board/src/app/app.component.ts
+++ kanban-board/src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { CdkDragDrop, transferArrayItem, CdkDrag } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
 import { Task } from './task/task';
+import { MatDialog } from '@angular/material/dialog';
+import { TaskDialogComponent, TaskDialogResult } from './task-dialog/task-dialog.component';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -14,6 +17,10 @@ export class AppComponent {
   inProgress: Task[] = [];
   done: Task[] = [];
 
+  constructor(private dialog: MatDialog) {
+
+  }
+
   drop(event: CdkDragDrop<Task[]>): void {
     if(event.previousContainer === event.container) {
       return;
@@ -26,7 +33,36 @@ export class AppComponent {
     )
   }
 
-  edit(list:string, task:Task): void {
+  edit(list:'done'|'todo'|'inProgress', task:Task): void {
+    const dialogRef= this.dialog.open(TaskDialogComponent,{
+      width: '270px',
+      data: {
+        task,
+        enableDelete:true
+      }
+    } );
+    dialogRef.afterClosed().subscribe((result: TaskDialogResult) => { 
+        const dataList =this[list];
+        const taskIndex =   dataList.indexOf(task);
+        if(result.delete){
+          dataList.splice(taskIndex,1);
+        }
+          else{
+            dataList[taskIndex]=task;
+          }
+        }
+      );
 
+  }
+  newTask() : void{
+    const dialogRef = this.dialog.open(TaskDialogComponent, {
+      width: '270px',
+      data: {
+        task: {}
+      }
+    } );
+    dialogRef
+      .afterClosed()
+      .subscribe((result: TaskDialogResult) => { if (result.task.title!= null || result.task.title!= null) {this.todo.push(result.task)}} );
   }
 }
